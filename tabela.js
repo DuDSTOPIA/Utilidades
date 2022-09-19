b3d2489eaab4cf43d270ed1ee6296eff1255da0f
unknown <dudscpa@hotmail.com>
2022-09-19 13:36:34 -0300

    começando um projeto de criação de tabelas

--- tabela.js
+++ tabela.js
@@ -1,29 +1,39 @@
-let seletor = document.querySelector('table')
 
 
+tabela('Eduardo', 'CPF', 'RG', 'DN');
 
 
+function tabela(...arguments) {
 
-tabela()
+    //busca o seletor table no DOM//
+    let table = document.querySelector('table');
 
-
-function tabela() {
-
-    let table = document.createElement('table');
+    // Cria a thead e a tr na table//
     let thead = document.createElement('thead');
     let tr = document.createElement('tr')
-    let th = document.createElement('th')
+    
+    thead.appendChild(tr);
+    
+    // Repassa a funcao celula para cada item repassado no paramentro arguments//
+    arguments.forEach(celula);
+    
 
-    table.style.border = '2px solid black'
-    table.style.fontSize = '16px';
+    
+    function celula(item) {
+        let th = document.createElement('th');
 
-    table.appendChild(thead);
-    thead.appendChild(tr)
-    tr.appendChild(th)
+        // estilos da tabela e das celulas 
+        table.style.margin = 'auto auto'
+        th.style.border = '2px solid black';
+        th.style.fontSize = '16px';
+        th.style.padding = '1em 2em'
 
-    th.textContent = 'texto'
-
-    seletor.appendChild(table)
-
-    console.log('teste de commit')
+        tr.appendChild(th)
+        th.textContent = item
+        console.log(item)        
+    }
+    
+    
+    
+    table.appendChild(thead)
 }
