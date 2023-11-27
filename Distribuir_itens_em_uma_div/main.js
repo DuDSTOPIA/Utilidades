f493a4e5782b07151ffa1974a3b69533e0e9d64f
Eduardo Domiciano <dudscpa@hotmail.com>
2023-11-27 18:50:44 -0300

    Transformado o sistema de distribuição de blocos em uma section em uma função

--- Distribuir_itens_em_uma_div/main.js
+++ Distribuir_itens_em_uma_div/main.js
@@ -1,4 +1,5 @@
 
+
 var lista = [
     {nome:'Eduardo', sobrenome:'Domiciano', idade: 30},
     {nome:'Carlos', sobrenome:'Oliveira', idade: 31},
@@ -8,17 +9,20 @@ var lista = [
     {nome:'Cleare', sobrenome:'Redfield', idade: 35},
     {nome:'Ada', sobrenome:'Wong', idade: 36},
 ]
-section = document.querySelector('.distribuição')
 
-lista.forEach(item => {
-    let div = document.createElement('div')
-    let model = `<div class='modelo'>
-                    <h1 class='item'>Nome: ${item.nome}</h1>
-                    <h1 class='item'>Sobrenome: ${item.sobrenome}</h1>
-                    <h1 class='item'>idade: ${item.idade}</h1>
-                </div>`
-    section.appendChild(div)
-    div.innerHTML = model
-
-    
-})
+function distribuicao_de_blocos_em_uma_section(list) {
+    section = document.querySelector('.distribuição')
+    lista.forEach(item => {
+        let div = document.createElement('div')
+        // Esse é o modelo HTML que será replicado
+        let model = `<div class='modelo'>
+                        <h1 class='item'>Nome: ${item.nome}</h1>
+                        <h1 class='item'>Sobrenome: ${item.sobrenome}</h1>
+                        <h1 class='item'>idade: ${item.idade}</h1>
+                    </div>`
+        section.appendChild(div)
+        div.innerHTML = model
+    })
+}
+//chamando a função
+distribuicao_de_blocos_em_uma_section(lista)
